00a48eed0453ac75decb79faaeea391edd694b31
Daniel Lozier <dan@dloz.net>
2016-07-20 23:19:39 -0400

    Centered jeopardy board items, added borders

--- src/main/java/xyz/triviopoly/view/game/JeopardyPanel.java
+++ src/main/java/xyz/triviopoly/view/game/JeopardyPanel.java
@@ -3,9 +3,12 @@ package xyz.triviopoly.view.game;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class JeopardyPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
@@ -15,33 +18,42 @@ public class JeopardyPanel extends JPanel {
 
 	public JeopardyPanel() {
 		setLayout(new GridBagLayout());
-		setBackground(Color.WHITE);
+		setBorder(BorderFactory.createLineBorder(Color.GREEN, 3));
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.BOTH;
 
 		c.gridy = 0;
+		c.insets = new Insets(0, 0, 0, 0);
 		c.weightx = 1;
-		c.weighty = 1;
+		c.weighty = 4;
 		for (int i = 0; i < 5; i++) {
 			c.gridx = i;
 			JLabel categoryNumber = new JLabel("" + (i + 1));
 			categoryNumber.setOpaque(true);
+			categoryNumber.setBorder(BorderFactory.createLineBorder(
+					Color.GREEN, 3));
 			categoryNumber.setBackground(Color.BLUE);
 			categoryNumber.setForeground(Color.GREEN);
+			categoryNumber.setHorizontalAlignment(SwingConstants.CENTER);
+			categoryNumber.setVerticalAlignment(SwingConstants.CENTER);
 			add(categoryNumber, c);
 		}
-		c.weighty = 2;
+		c.weighty = 8;
 		c.gridy = 1;
 		categories = new JLabel[5];
 		for (int i = 0; i < 5; i++) {
 			c.gridx = i;
 			categories[i] = new JLabel("Category " + (i + 1));
 			categories[i].setOpaque(true);
+			categories[i].setBorder(BorderFactory.createLineBorder(Color.GREEN,
+					3));
 			categories[i].setBackground(Color.BLUE);
 			categories[i].setForeground(Color.GREEN);
+			categories[i].setHorizontalAlignment(SwingConstants.CENTER);
+			categories[i].setVerticalAlignment(SwingConstants.CENTER);
 			add(categories[i], c);
 		}
-		c.weighty = 3;
+		c.weighty = 16;
 		questions = new JLabel[5][5];
 		for (int i = 0; i < 5; i++) {
 			questions[i] = new JLabel[5];
@@ -50,8 +62,12 @@ public class JeopardyPanel extends JPanel {
 				c.gridy = i + 2;
 				questions[i][j] = new JLabel("$" + ((i + 1) * 100));
 				questions[i][j].setOpaque(true);
+				questions[i][j].setBorder(BorderFactory.createLineBorder(
+						Color.GREEN, 3));
 				questions[i][j].setBackground(Color.BLUE);
 				questions[i][j].setForeground(Color.GREEN);
+				questions[i][j].setHorizontalAlignment(SwingConstants.CENTER);
+				questions[i][j].setVerticalAlignment(SwingConstants.CENTER);
 				add(questions[i][j], c);
 			}
 		}
